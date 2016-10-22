e0c2f7cd17fd33e09584e51c023ae8a9ca5f367f
pablogarciabenayas <pablogarciabenayas@gmail.com>
2016-10-22 17:03:32 +0200

    Agregados comentarios sobre libreria

--- libreria.h
+++ libreria.h
@@ -1,4 +1,22 @@
-/*Cabeceras de libreria*/
-	int head(int n);
-	int tail(int n);
-	int longlines(int n);
+/*
+ * Libreria práctica 1 de sistemas operativos
+ * 
+ * */
+
+
+/* Lectura de la entrada estándar, muestra las n primeras líneas en la 
+ * salida estándar recibidas por la entrada estándar.
+ */
+int head(int n);
+
+/* Lectura de la entrada estándar, muestra las n últimas líneas en la
+ * salida estándar recibidas por la entrada estándar.
+ */ 
+int tail(int n);
+
+/*
+ * Muestra las n líneas más largas recibidas de mayor a menor longitud,
+ * o todas si hay menos de n líneas por la salida estándar recibidas
+ * por la entrada estándar. 
+ */
+int longlines(int n);
