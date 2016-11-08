86d662c0bc31d2e51d37d62b32d34fc7a17a34b7
pablogarciabenayas <pablogarciabenayas@gmail.com>
2016-11-08 13:53:31 +0100

    Cambios en test.c, mejorada la gestion de parametros, ahora los parametros son así: ./test {head/tail/longlines} -N

--- test.c
+++ test.c
@@ -8,26 +8,32 @@
 */
 
 int error(char *value){
-	printf("Error, argumentos no validos.Uso:\n\t%s head Num_lineas\n\t%s tail Num_lineas\n\t%s longlines Num_lineas\nNum_lineas debe ser el numero de lineas que desea\n",value,value,value);
+	printf("Error, argumentos no validos.Uso:\n\t%s head -N\n\t%s tail -N\n\t%s longlines -N\nN debe ser el numero de lineas que desea\n",value,value,value);
 	return 1;
 }
 
 int main(int argc, char *argv[]){
 
 	int lines;
-
+	char *num;
+	
 	switch(argc){
 		case 3:
-			lines = atoi(argv[2]);
+			num = argv[2]+1;
+			if(strcmp(num,"\0")!=0){
+				lines = atoi(num);
 			
-			if(strcmp(argv[1],"head")==0){
-				head(lines);
-			}else if(strcmp(argv[1],"tail")==0){
-				tail(lines);
-			}else if(strcmp(argv[1],"longlines")==0){
-				longlines(lines);
+				if(strcmp(argv[1],"head")==0){
+					head(lines);
+				}else if(strcmp(argv[1],"tail")==0){
+					tail(lines);
+				}else if(strcmp(argv[1],"longlines")==0){
+					longlines(lines);
+				}else{
+					return error(argv[0]);
+				}
 			}else{
-				return error(argv[0]);
+				printf("Error, el parametro -N no es valido, introduzca un numero de lineas valido.\n");
 			}
 		break;
 		default:
